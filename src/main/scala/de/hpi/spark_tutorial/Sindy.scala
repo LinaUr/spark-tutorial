7e22e24978f72733eaaca4df91bd41cda1051ed5
Leonard Kulms <leonard.kulms@icloud.com>
2021-07-03 14:40:23 +0200

    current state (pre-pairprogramming)

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -24,7 +24,7 @@ object Sindy {
       .option("header", "true")
       .csv(table)
     )
-    // dfs.foreach(table => print(table.show()))
+    //dfs.foreach(table => print(table.show()))
 
     // TODO 1st step: -> "cells": zip every value of every cell with column name
     //  example: [("Thriller", (a)), ("Thriller", (t)), ("Thriller", (p))]
@@ -33,17 +33,19 @@ object Sindy {
       df.flatMap(row => row.toSeq.map(p => String.valueOf(p)).zip(columns))
     })
 
-    //cells.take(1).foreach(cell => print(cell.show()))
+    cells.take(2).foreach(cell => print(cell.show()))
 
     // TODO 2nd step: -> "cache-based preaggr.": pre-aggregate all values that occur multiple times so that:
     //  (("Thriller", (a, t)) in worker 1, ("Thriller", (p)) in worker 2
 
+    cells.map(cell => cell.groupByKey(cell => cell._1))
+
     // TODO 3rd step: -> "global partitioning & attribute sets": bring same values from different workers
     //  to the same workers and create sets of columns with potential INDs; example: everything with value "Thriller"
-    //  is in the same worker, worker creates: {a,t,p}
+    //  is in the same worker, worker creates: {a,t,p} {c}
 
     // TODO 4th step: -> "inclusion lists": from every set, create a list of possible inclusion dependencies:
-    //  [(a, {t,p}), (t, {a,p}), (p,{t,a})].
+    //  [(a, {t,p}), (t, {a,p}), (p,{t,a})]. [c]
     //  note: also create lists with length 1, if a value only occurs in one specific column
 
     // TODO 5th step: -> "partition": match inclusion lists to workers so that a set of tables is only checked by
