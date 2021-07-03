8d370e11671afa44b71359f762845f4048e86c3d
Lina Urban <lina.urban@student.hpi.de>
2021-07-03 20:10:09 +0200

    add step 3, and broken step 4

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,6 +1,6 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 
 object Sindy {
 
@@ -33,20 +33,42 @@ object Sindy {
       df.flatMap(row => row.toSeq.map(p => String.valueOf(p)).zip(columns))
     })
 
-    cells.take(2).foreach(cell => print(cell.show()))
+//    cells.take(2).foreach(cell => print(cell.show()))
 
     // TODO 2nd step: -> "cache-based preaggr.": pre-aggregate all values that occur multiple times so that:
     //  (("Thriller", (a, t)) in worker 1, ("Thriller", (p)) in worker 2
 
-    cells.map(cell => cell.groupByKey(cell => cell._1))
+    //val grouped = cells.map(cell => cell.groupByKey(cell => cell._1))
+
+    val KVGDs = cells.map(cell => cell.groupByKey(_._1))//.mapGroups{case(k, iter) => (k, iter.map(x => x._2).toArray)}.show
 
     // TODO 3rd step: -> "global partitioning & attribute sets": bring same values from different workers
     //  to the same workers and create sets of columns with potential INDs; example: everything with value "Thriller"
     //  is in the same worker, worker creates: {a,t,p} {c}
 
+    val aggregatedDs = KVGDs.combinations(2).toList
+                            .map(kvgvList => kvgvList.head.cogroup(kvgvList.tail.head)((key:String, it1:Iterator[(String,String)], it2:Iterator[(String,String)]) => it1 ++ it2))
+//    aggregatedDs.foreach(a => a.show())
+
+    val attributeDfs = aggregatedDs
+              .map(aggDs => aggDs.groupByKey(_._1).mapGroups{case(group, values) => (group, values.map(v => v._2).toSet)})
+              .map(_.drop("_1").toDF()) // drop the keys
+
+
+    // join all DFs (which represent a single attribute row) to a single DF
+    var attributeSets = attributeDfs.tail.foldLeft(attributeDfs.head)((accDF, newDF) => accDF.join(newDF, Seq("_2")))
+    attributeSets = attributeSets.distinct()
+
+    attributeSets.show()
+
+
     // TODO 4th step: -> "inclusion lists": from every set, create a list of possible inclusion dependencies:
     //  [(a, {t,p}), (t, {a,p}), (p,{t,a})]. [c]
     //  note: also create lists with length 1, if a value only occurs in one specific column
+    /**/
+    val inclSets = attributeSets.map(row => row.toSeq.head.asInstanceOf[List[String]])
+                  .flatMap(list => list.permutations.map(permList => (permList.head, permList.tail.toSet)))
+    inclSets.foreach(print(_))
 
     // TODO 5th step: -> "partition": match inclusion lists to workers so that a set of tables is only checked by
     //  a particular worker
